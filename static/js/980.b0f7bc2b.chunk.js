9dcc01ad090692e16ca870ed7c2a6aa525f75689
Holms85 <94076735+Holms85@users.noreply.github.com>
2022-09-25 16:53:56 +0000

    Deploying to gh-pages from @ Holms85/goit-react-hw-08-phonebook@2b0ef8b577701d559fcaa54f682568e8fff5ca00 🚀

---
+++ static/js/980.b0f7bc2b.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[980],{9980:function(e,n,t){t.r(n),t.d(n,{default:function(){return c}});var r=t(1087),a=t(184);function c(){return(0,a.jsxs)("div",{className:"container",children:[(0,a.jsx)("h1",{children:"Page not found"}),(0,a.jsx)(r.rU,{to:"/",children:"Back to Main page"})]})}}}]);
+//# sourceMappingURL=980.b0f7bc2b.chunk.js.map
